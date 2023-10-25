2fe1865e5d2d4f4927e16e9a519702506d336eba
leoshen <chengzhi.shen@outlook.com>
2023-10-25 14:39:30 +0200

    hydra added

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -6,24 +7,30 @@ from lightning_module import LandmarkDetector
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 
-def build_callbacks():
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+
+def build_callbacks(cfg):
+    
+    dirpath = os.path.join(*['checkpoints', cfg.dimension, cfg.cnn_type])
     checkpoint_callback = ModelCheckpoint(
         monitor='val.loss',
         mode='min',
-        dirpath='checkpoints',
-        filename='{epoch:02d}-{val.acc_top5:.2f}',
+        dirpath=dirpath,
+        filename='{epoch:02d}',
         save_top_k=1,
         save_last=True,
         )
     
-    # early_stop_callback = EarlyStopping(
-    #     monitor='val.loss',
-    #     mode='min',
-    #     patience=5,
-    # )
+    early_stop_callback = EarlyStopping(
+        monitor='val.loss',
+        mode='min',
+        patience=10,
+    )
     
     callbacks = [checkpoint_callback, 
-                #  early_stop_callback,
+                 early_stop_callback,
                  ]
     return callbacks
 
@@ -34,15 +41,16 @@ def build_transforms():
     
     return t
 
-
-def train():
-    max_epochs = 50
-    batch_size = 1
-    logger = WandbLogger(project='landmark_detection', name='2D landmark')
-    # logger = None
+@hydra.main(config_path="config", config_name="2d")
+def train(cfg: DictConfig):
+    max_epochs = cfg.max_epochs
+    batch_size = cfg.batch_size
     transforms = build_transforms()
     
-    model = LandmarkDetector(cnn_type='custom')    
+    logger = WandbLogger(project='landmark_detection', name='2D_'+ cfg.cnn_type)
+    # logger = None
+    
+    model = LandmarkDetector(cnn_type=cfg.cnn_type, output_dim=2)    
     
     train_loader = DataLoader(
         LandmarkDataset(name_point='S_Point', stage='train', size_image=520, transform=transforms),
@@ -51,26 +59,26 @@ def train():
         num_workers=0,
         pin_memory=False
     )
-    # val_dataloader = DataLoader(
-    #     LandmarkDataset(name_point='S_Point', stage='val', size_image=520, transform=transforms),
-    #     batch_size=batch_size,
-    #     shuffle=False,
-    #     num_workers=0,
-    #     pin_memory=False
-    # )
+    val_dataloader = DataLoader(
+        LandmarkDataset(name_point='S_Point', stage='val', size_image=520, transform=transforms),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=False
+    )
     
     trainer = pl.Trainer(
         accelerator='mps',
         max_epochs=max_epochs,
         log_every_n_steps=1,
         precision=32,
-        callbacks=build_callbacks(),
+        callbacks=build_callbacks(cfg),
         logger=logger,
         )    
 
     trainer.fit(model, 
                 train_dataloaders=train_loader, 
-                # val_dataloaders=val_dataloader
+                val_dataloaders=val_dataloader
                 )
     
 
